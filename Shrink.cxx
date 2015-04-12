f3fafca0d7b2766cdace12d05169698774abef05
Cory Bloor <cgbloor@ucalgary.ca>
2015-04-12 01:25:20 -0600

    Major optimization
    When doing template-matching on a 3x3 neighbourhood, all templates
    have the center bit set. That leaves the 8 surrounding pixels to
    check.
    
    Rather than check the pixel neighbourhood against each template,
    I built a pair of lookup tables for the conditional and unconditional
    templates. There are only 256 possiblilities for an 8-pixel
    configuration, so each lookup table can be crammed into 4 64-bit
    integers.
    
    This results in a >10x performance gain.
    
    Benchmark:
    Before: 3.11s
    After:  0.22s

--- Shrink.cxx
+++ Shrink.cxx
@@ -4,451 +4,101 @@
 */
 #include <assert.h>
 #include <string.h>
+#include <stdint.h>
 #include <opencv2/core/core.hpp>
 #include "Shrink.h"
 
 namespace cgmb {
 
-// the value which represents black in the binary image
-static const unsigned char I = 255;
-
-// Digital Image Processing, 3rd Edition, Table 14.3-1, pp.413,414
-// Shrink, Thin, and Skeletonize Conditional Mark Patterns [M = 1 if hit]
-static const unsigned char k_conditional_patterns[][9] = {
-// S 1
-{0,0,I,
- 0,I,0,
- 0,0,0},
-{I,0,0,
- 0,I,0,
- 0,0,0},
-{0,0,0,
- 0,I,0,
- I,0,0},
-{0,0,0,
- 0,I,0,
- 0,0,I},
-
-// S 2
-{0,0,0,
- 0,I,I,
- 0,0,0},
-{0,I,0,
- 0,I,0,
- 0,0,0},
-{0,0,0,
- I,I,0,
- 0,0,0},
-{0,0,0,
- 0,I,0,
- 0,I,0},
-
-// S 3
-{0,0,I,
- 0,I,I,
- 0,0,0},
-{0,I,I,
- 0,I,0,
- 0,0,0},
-{I,I,0,
- 0,I,0,
- 0,0,0},
-{I,0,0,
- I,I,0,
- 0,0,0},
-{0,0,0,
- I,I,0,
- I,0,0},
-{0,0,0,
- 0,I,0,
- I,I,0},
-{0,0,0,
- 0,I,0,
- 0,I,I},
-{0,0,0,
- 0,I,I,
- 0,0,I},
-/*
-// TK 4
-{0,I,0,
- 0,I,I,
- 0,0,0},
-{0,I,0,
- I,I,0,
- 0,0,0},
-{0,0,0,
- I,I,0,
- 0,I,0},
-{0,0,0,
- 0,I,I,
- 0,I,0},
-*/
-// STK 4
-{0,0,I,
- 0,I,I,
- 0,0,I},
-{I,I,I,
- 0,I,0,
- 0,0,0},
-{I,0,0,
- I,I,0,
- I,0,0},
-{0,0,0,
- 0,I,0,
- I,I,I},
-
-// ST 5
-{I,I,0,
- 0,I,I,
- 0,0,0},
-{0,I,0,
- 0,I,I,
- 0,0,I},
-{0,I,I,
- I,I,0,
- 0,0,0},
-{0,0,I,
- 0,I,I,
- 0,I,0},
-
-// ST 5
-{0,I,I,
- 0,I,I,
- 0,0,0},
-{I,I,0,
- I,I,0,
- 0,0,0},
-{0,0,0,
- I,I,0,
- I,I,0},
-{0,0,0,
- 0,I,I,
- 0,I,I},
-
-// ST 6
-{I,I,0,
- 0,I,I,
- 0,0,I},
-{0,I,I,
- I,I,0,
- I,0,0},
-
-// STK 6
-{I,I,I,
- 0,I,I,
- 0,0,0},
-{0,I,I,
- 0,I,I,
- 0,0,I},
-{I,I,I,
- I,I,0,
- 0,0,0},
-{I,I,0,
- I,I,0,
- I,0,0},
-{I,0,0,
- I,I,0,
- I,I,0},
-{0,0,0,
- I,I,0,
- I,I,I},
-{0,0,0,
- 0,I,I,
- I,I,I},
-{0,0,I,
- 0,I,I,
- 0,I,I},
-
-// STK 7
-{I,I,I,
- 0,I,I,
- 0,0,I},
-{I,I,I,
- I,I,0,
- I,0,0},
-{I,0,0,
- I,I,0,
- I,I,I},
-{0,0,I,
- 0,I,I,
- I,I,I},
-
-// STK 8
-{0,I,I,
- 0,I,I,
- 0,I,I},
-{I,I,I,
- I,I,I,
- 0,0,0},
-{I,I,0,
- I,I,0,
- I,I,0},
-{0,0,0,
- I,I,I,
- I,I,I},
-
-// STK 9
-{I,I,I,
- 0,I,I,
- 0,I,I},
-{0,I,I,
- 0,I,I,
- I,I,I},
-{I,I,I,
- I,I,I,
- I,0,0},
-{I,I,I,
- I,I,I,
- 0,0,I},
-{I,I,I,
- I,I,0,
- I,I,0},
-{I,I,0,
- I,I,0,
- I,I,I},
-{I,0,0,
- I,I,I,
- I,I,I},
-{0,0,I,
- I,I,I,
- I,I,I},
-
-// STK 10
-{I,I,I,
- 0,I,I,
- I,I,I},
-{I,I,I,
- I,I,I,
- I,0,I},
-{I,I,I,
- I,I,0,
- I,I,I},
-{I,0,I,
- I,I,I,
- I,I,I},
-/*
-// K 11
-{I,I,I,
- I,I,I,
- 0,I,I},
-{I,I,I,
- I,I,I,
- I,I,0},
-{I,I,0,
- I,I,I,
- I,I,I},
-{0,I,I,
- I,I,I,
- I,I,I},
-*/
-};
-
-// Digital Image Processing, 3rd Edition, Table 14.3-I, p. 414
-// Shrink, Thin, and Skeletonize Unconditional Mark Patterns
-// P(M,M0,M1,M2,M3,M4,M5,M6,M7) = 1 if hit]
-
-// A ∪ B ∪ C = 1
-// A ∪ B = 1
-// D = 0 ∪ 1
 static const unsigned char M = 1;
-static const unsigned char A = 2;
-static const unsigned char B = 3;
-static const unsigned char C = 4;
-static const unsigned char D = 5;
 
-static const unsigned char k_unconditional_patterns[][9] = {
-// spur
-{0,0,M,
- 0,M,0,
- 0,0,0},
-{M,0,0,
- 0,M,0,
- 0,0,0},
-{0,0,0,
- 0,M,0,
- 0,M,0},
-{0,0,0,
- 0,M,M,
- 0,0,0},
-
-// L Cluster
-{0,0,M,
- 0,M,M,
- 0,0,0},
-{0,M,M,
- 0,M,0,
- 0,0,0},
-{M,M,0,
- 0,M,0,
- 0,0,0},
-{M,0,0,
- M,M,0,
- 0,0,0},
-{0,0,0,
- M,M,0,
- M,0,0},
-{0,0,0,
- 0,M,0,
- M,M,0},
-{0,0,0,
- 0,M,0,
- 0,M,M},
-{0,0,0,
- 0,M,M,
- 0,0,M},
-
-// offset
-{0,M,M,
- M,M,0,
- 0,0,0},
-{M,M,0,
- 0,M,M,
- 0,0,0},
-{0,M,0,
- 0,M,M,
- 0,0,M},
-{0,0,M,
- 0,M,M,
- 0,M,0},
-
-// spur corner
-{0,A,M,
- 0,M,B,
- M,0,0},
-{M,B,0,
- A,M,0,
- 0,0,M},
-{0,0,M,
- A,M,0,
- M,B,0},
-{M,0,0,
- 0,M,B,
- 0,A,M},
-
-// corner clutter
-{M,M,D,
- M,M,D,
- D,D,D},
-
-// tee branch
-{D,M,0,
- M,M,M,
- D,0,0},
-{0,M,D,
- M,M,M,
- 0,0,D},
-{0,0,D,
- M,M,M,
- 0,M,D},
-{D,0,0,
- M,M,M,
- D,M,0},
-{D,M,D,
- M,M,0,
- 0,M,0},
-{0,M,0,
- M,M,0,
- D,M,D},
-{0,M,0,
- 0,M,M,
- D,M,D},
-{D,M,D,
- 0,M,M,
- 0,M,0},
-
-// vee branch
-{M,D,M,
- D,M,D,
- A,B,C},
-{M,D,C,
- D,M,B,
- M,D,A},
-{C,B,A,
- D,M,D,
- M,D,M},
-{A,D,M,
- B,M,D,
- C,D,M},
-
-// diagonal branch
-{D,M,0,
- 0,M,M,
- M,0,D},
-{0,M,D,
- M,M,0,
- D,0,M},
-{D,0,M,
- M,M,0,
- 0,M,D},
-{M,0,D,
- 0,M,M,
- D,M,0},
-};
-
-static bool match(const unsigned char d[9], const unsigned char t[9]) {
-  bool need_abc = false;
-  bool a = false;
-  bool b = false;
-  bool c = false;
-  for (size_t i = 0; i < 9; ++i) {
-    switch(t[i]) {
-      case 0:
-        if (d[i] == M) {
-          return false;
-        }
-        break;
-      case M:
-        if (d[i] == 0) {
-          return false;
-        }
-        break;
-      case A:
-        if (d[i] == M) {
-          a = true;
-        } else {
-          need_abc = true;
-        }
-        break;
-      case B:
-        if (d[i] == M) {
-          b = true;
-        } else {
-          need_abc = true;
-        }
-        break;
-      case C:
-        if (d[i] == M) {
-          c = true;
-        } else {
-          need_abc = true;
-        }
-        break;
-      case D:
-        break;
-      default:
-        assert(false);
-        break;
-    }
-  }
-  return !need_abc || (a || b || c);
+static bool test_flag(const uint64_t* flags, unsigned char i) {
+  unsigned char j = i / 64u;
+  unsigned char k = i % 64u;
+  uint64_t bitmask = uint64_t(1) << k;
+  return flags[j] & bitmask;
 }
 
-static void fill_pixel_stack(const cv::Mat& in,
-  const int rows, const int cols, const int row, const int col,
-  unsigned char stack[9]) {
-  // gather the pixels to compare
-  size_t i = 0;
-  for (int dr = -1; dr <= 1; ++dr) {
-    for (int dc = -1; dc <= 1; ++dc) {
-      if (row + dr < 0 || row + dr == rows ||
-          col + dc < 0 || col + dc == cols) {
-        stack[i] = 0;
-      } else {
-        stack[i] = in.at<unsigned char>(row + dr, col + dc);
+static bool is_set(const cv::Mat& in, int row, int col) {
+  return in.at<unsigned char>(row, col) != 0;
+}
+
+static unsigned char pattern_index(const cv::Mat& in,
+  const int rows, const int cols, const int row, const int col) {
+  assert(rows > 0);
+  assert(cols > 0);
+  assert(row >= 0);
+  assert(col >= 0);
+  assert(row < rows);
+  assert(col < cols);
+
+  unsigned char v = 0;
+  if (row > 0) {
+    if (col > 0) {
+      if (is_set(in, row - 1, col - 1)) {
+        v += (1 << 0);
       }
-      ++i;
+    }
+    if (is_set(in, row - 1, col)) {
+      v += (1 << 1);
+    }
+    if (col < cols - 1) {
+      if (is_set(in, row - 1, col + 1)) {
+        v += (1 << 2);
+      }
     }
   }
+
+  if (col > 0) {
+    if (is_set(in, row, col - 1)) {
+      v += (1 << 3);
+    }
+  }
+  if (col < cols - 1) {
+    if (is_set(in, row, col + 1)) {
+      v += (1 << 4);
+    }
+  }
+
+  if (row < rows - 1) {
+    if (col > 0) {
+      if (is_set(in, row + 1, col - 1)) {
+        v += (1 << 5);
+      }
+    }
+    if (is_set(in, row + 1, col)) {
+      v += (1 << 6);
+    }
+    if (col < cols - 1) {
+      if (is_set(in, row + 1, col + 1)) {
+        v += (1 << 7);
+      }
+    }
+  }
+
+  return v;
+}
+
+static const uint64_t k_conditional[] = {
+  0x8000cb0180d9cbde,
+  0x00008b0100100001,
+  0x8000000080dd0001,
+  0x33d18b0100d10001,
+};
+
+static const uint64_t k_unconditional[] = {
+  0xaefeb9e0cc19ca5a,
+  0xabaebcb1bbfefca1,
+  0xfefefefefca7fea8,
+  0xfefefefef9a6fca3,
+};
+
+static bool check_conditional_match(unsigned char index) {
+  return test_flag(k_conditional, index);
+}
+
+static bool check_unconditional_match(unsigned char index) {
+  return test_flag(k_unconditional, index);
 }
 
 // input:  CV_8UC1
@@ -457,7 +107,6 @@ cv::Mat shrink_max(cv::Mat in) {
   const int rows = in.size().height;
   const int cols = in.size().width;
 
-  unsigned char stack[9];
   size_t erasure_count;
   do {
     erasure_count = 0;
@@ -468,15 +117,9 @@ cv::Mat shrink_max(cv::Mat in) {
           continue; // this one can't be erased -- it's already empty!
         }
 
-        fill_pixel_stack(in, rows, cols, row, col, stack);
-
-        // check for a conditional match
-        for (size_t i = 0; i < sizeof(k_conditional_patterns)/9; ++i) {
-          int diff = memcmp(&k_conditional_patterns[i][0], stack, 9);
-          if (diff == 0) {
-            conditional.at<unsigned char>(row, col) = M;
-            break;
-          }
+        unsigned char p = pattern_index(in, rows, cols, row, col);
+        if (check_conditional_match(p)) {
+          conditional.at<unsigned char>(row, col) = M;
         }
       }
     }
@@ -489,13 +132,9 @@ cv::Mat shrink_max(cv::Mat in) {
           continue; // no need to bother if already 0
         }
 
-        fill_pixel_stack(conditional, rows, cols, row, col, stack);
-
-        for (size_t i = 0; i < sizeof(k_unconditional_patterns)/9; ++i) {
-          if (match(stack, &k_unconditional_patterns[i][0])) {
-            out.at<unsigned char>(row, col) = 0;
-            break;
-          }
+        unsigned char p = pattern_index(conditional, rows, cols, row, col);
+        if (check_unconditional_match(p)) {
+          out.at<unsigned char>(row, col) = 0;
         }
       }
     }
